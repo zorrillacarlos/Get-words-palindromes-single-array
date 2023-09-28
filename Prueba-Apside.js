5cd34adae0ca4a36fa6febf1b6b13a12c5da7c6f
zorrillacarlos <zorrillacarlos4@gmail.com>
2023-09-28 07:05:39 -0400

    Update Prueba-Apside.js
    update function

--- Prueba-Apside.js
+++ Prueba-Apside.js
@@ -1,5 +1,5 @@
 // Palabra Palindrome
-function getPalindrome(str) {
+const getPalindrome = (str) =>{
   let strReverse = str.split("").reverse().join("");
   if (str.toLowerCase() === strReverse.toLowerCase()) {
     return `${str} es palindrome`;
@@ -11,30 +11,17 @@ console.log(getPalindrome("oso"));
 
 // Arreglo Palabras Palindrome
 
-function getPalindromesArray(arrayPalindrome) {
-    return arrayPalindrome.filter(element => {
-    const originalWord = element.toLowerCase().slice(0, Math.ceil(element.length/2));
-    const reverseWord = element.toLowerCase().slice(Math.floor(element.length/2)).split('').reverse().join('');
-    return originalWord === reverseWord;
-    });
-}
-
-const wordsArr = ['1001', 'oso', 'Otto', 'Ana', 'Juan'];
-
-console.log(getPalindromesArray(wordsArr));
-
-// Arreglo Palabras Palindrome For
-
-function getPalindromes(wordsArr) {
-  const wordsPalindromeTrue = [];
-  for (let i = 0; i < wordsArr.length; i++) {
-    if (
-      wordsArr[i].toLowerCase() ===
-      wordsArr[i].split("").reverse().join("").toLowerCase()
-    ) {
-      wordsPalindromeTrue.push(` ${wordsArr[i]}`);
-    }
+const palabrasPalindromas = (palabras) => {
+  const esPalindromo = (palabra) => {
+    palabra = palabra.toLowerCase().replace(/ /g, '');
+    return palabra === palabra.split('').reverse().join('');
   }
-  return console.log(`las palabras palindromas en el array son:${wordsPalindromeTrue}`);
+  // Filtra el array de palabras y devuelve solo las palíndromos
+  return palabras.filter(palabra => {
+    return esPalindromo(palabra);
+  });
 }
-getPalindromes(wordsArr);
+
+let listaDePalabras = ["reconocer", "oso", "casa", "anilina", "Coc "];
+let palindromos = palabrasPalindromas(listaDePalabras);
+console.log("Palíndromos encontrados:", palindromos);
